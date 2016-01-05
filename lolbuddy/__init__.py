d7ffbf979bbacc3b84455b0385b118da1c996c4a
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-01-05 16:26:48 -0600

    all that's left is making the files

--- lolbuddy/__init__.py
+++ lolbuddy/__init__.py
@@ -4,106 +4,109 @@ from requests_futures.sessions import FuturesSession
 import sys
 import os
 import pickle
+#import pprint
+#pprint used for debug, doesn't need to be used for production
 
 def main():
 
-    #for giving a clean exit
-    try:
-        if ('--reset' in sys.argv or '-r' in sys.argv):
-            try:
-                os.remove('.lolbuddy')
-            except:
-                print('\nThere was no pre-existing config file anyway...\n')
-
-        #don't want to write to file if we don't need to
-        writeToFile = False
-
+    if ('--reset' in sys.argv or '-r' in sys.argv):
         try:
-            data = pickle.load(open('.lolbuddy', 'rb'))
-            location = data['location']
-            apiKey = data['apiKey']
+            os.remove('.lolbuddy')
         except:
-            writeToFile = True
-            data = {'location': '', 'apiKey': ''}
-            location = ''
-            apiKey = ''
+            print('\nThere was no pre-existing config file anyway...\n')
 
-        #Check if the .lolbuddy file either doesn't have the location saved or doesn't have the correct location saved
-        if (location == '' or not os.path.exists(location)):
-            #otherwise, check if the location is one of the defaults for the OS
+    #don't want to write to file if we don't need to
+    writeToFile = False
+
+    try:
+        data = pickle.load(open('.lolbuddy', 'rb'))
+        location = data['location']
+        apiKey = data['apiKey']
+    except:
+        writeToFile = True
+        data = {'location': '', 'apiKey': ''}
+        location = ''
+        apiKey = ''
+
+    #Check if the .lolbuddy file either doesn't have the location saved or doesn't have the correct location saved
+    if (location == '' or not os.path.exists(location)):
+        #otherwise, check if the location is one of the defaults for the OS
+        writeLocation = True
+        if os.name == 'posix': #OS X
+            location = '/Applications/League of Legends.app/Contents/LoL/Config'
+        elif os.name == 'nt': #Windows
+            location = 'C:/Riot Games/League of Legends/Config'
+        else: #Linux
+            print('\nSorry, linux is not supported...\n')
+            quit()
+        #If the location path STILL isn't correct, prompt the user for the correct path
+        while not os.path.exists(location):
             writeLocation = True
-            if os.name == 'posix': #OS X
-                location = '/Applications/League of Legends.app/Contents/LoL/Config'
-            elif os.name == 'nt': #Windows
-                location = 'C:/Riot Games/League of Legends/Config'
-            else: #Linux
-                print('\nSorry, linux is not supported...\n')
-                quit()
-            #If the location path STILL isn't correct, prompt the user for the correct path
-            while not os.path.exists(location):
-                writeLocation = True
-                location = input('Could not find location of league config folder, please enter it here. It should look something like this "C:/Riot Games/League of Legends/Config" or "/Applications/League of Legends.app/Contents/LoL/Config":\n')
+            location = input('Could not find location of league config folder, please enter it here. It should look something like this "C:/Riot Games/League of Legends/Config" or "/Applications/League of Legends.app/Contents/LoL/Config":\n')
 
-        #if the API key isn't saved or clearly too short to be a champion.gg API key
-        if len(apiKey) < 20: #assuming consistency, champion.gg's is 32 chars
-            writeToFile = True
-            #keep prompting user until they get it right
-            while len(apiKey) < 20:
-                apiKey = input('Not a valid API key, please enter one from api.champion.gg: ')
+    #if the API key isn't saved or clearly too short to be a champion.gg API key
+    if len(apiKey) < 20: #assuming consistency, champion.gg's is 32 chars
+        writeToFile = True
+        #keep prompting user until they get it right
+        while len(apiKey) < 20:
+            apiKey = input('Not a valid API key, please enter one from api.champion.gg: ')
 
-        if writeToFile:
-            data = {'location': location, 'apiKey': apiKey}
-            print(data)
+    if writeToFile:
+        data = {'location': location, 'apiKey': apiKey}
+        print(data)
+        pickle.dump(data, open('.lolbuddy', 'wb'))
+
+    session = FuturesSession()
+
+    apiKeyValidated = False
+    while not apiKeyValidated:
+        url = 'http://api.champion.gg/champion?api_key=' + apiKey
+        infoRequest = session.get(url) #start request
+        #requestData = json.loads(infoRequest.result().content.decode('utf-8')) #finish request
+        requestData = infoRequest.result().content.decode('utf-8') #finish request
+        try:
+            champions = json.loads(requestData)
+            apiKeyValidated = True
+        except:
+            apiKey = input('Seems like the API key didn\'t work, please enter a valid API key: ')
+            data['apiKey'] = apiKey
             pickle.dump(data, open('.lolbuddy', 'wb'))
+    totalChampions = len(champions)
+    championsComplete = 0
+    print('\nThere are currently {0} champions. Getting individual champion data...\n'.format(totalChampions))
 
-        session = FuturesSession()
+    championInfo = []
 
-        apiKeyValidated = False
-        while not apiKeyValidated:
-            url = 'http://api.champion.gg/champion?api_key' + apiKey
-            infoRequest = session.get(url) #start request
-            #requestData = json.loads(infoRequest.result().content.decode('utf-8')) #finish request
-            requestData = infoRequest.result().content.decode('utf-8') #finish request
-            try:
-                champions = json.loads(requestData)
-                apiKeyValidated = True
-            except:
-                apiKey = input('Seems like the API key didn\'t work, please enter a valid API key')
-                data['apiKey'] = apiKey
-                pickle.dump(data, open('.lolbuddy', 'wb'))
-            print(champions)
-            input('')
-        totalChampions = len(champions)
-        championsComplete = 0
-        print('\nThere are currently {0} champions. Getting individual champion data...\n'.format(totalChampions))
+    def incrementChamps(sess, resp):
+        nonlocal championsComplete, championInfo
+        championsComplete += 1
+        champion = resp.content.decode('utf-8')
+        try:
+            championJson = json.loads(champion)
+            championInfo.append(championJson)
+        except:
+            print('Error while attempting to fetch champion {0}'.format(championsComplete))
 
-        championInfo = []
+    for champion in champions:
+        url = 'http://api.champion.gg/champion/' + champion['key'] + '?&api_key=' + apiKey
+        championData = session.get(url, background_callback=incrementChamps)
 
-        def incrementChamps(sess, resp):
-            nonlocal championsComplete
-            championsComplete += 1
+    while championsComplete < totalChampions:
+        print('({0}/{1}) champions complete...'.format(championsComplete, totalChampions), end='\r')
+        sys.stdout.flush()
 
-        for champion in champions:
-            url = 'http://api.champion.gg/champion/' + champion['key'] + '?&api_key=' + apiKey
-            championData = session.get(url, background_callback=incrementChamps)
-            championInfo.append(championData)
+    print('\n\nAll champion data fetched, creating item sets...')
 
-        while championsComplete < totalChampions:
-            print('({0}/{1}) champions complete...'.format(championsComplete, totalChampions), end='\r')
-            sys.stdout.flush()
+    championsComplete = 0
 
-        print('\n\nAll champion data fetched, creating item sets...')
+    for champion in championInfo:
+        champFolder = location + '/Champions/' + champion[0]['key']
+        if not os.path.exists(champFolder):
+            os.makedirs(champFolder)
 
-        for champion in championsInfo:
-            champFolder = location + champion['key']
-            if not os.path.exists(champFolder):
-                os.makedirs(directory)
-
-
-    except (KeyboardInterrupt, SystemExit):
-        print("\n\nShutdown requested...exiting")
-    quit()
-
+        championsComplete += 1
+        print('({0}/{1}) champions complete...'.format(championsComplete, totalChampions), end='\r')
+        sys.stdout.flush()
 
 
 if __name__ == '__main__':
