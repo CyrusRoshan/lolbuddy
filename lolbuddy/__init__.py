7e7822b9194274dc4d61478984f1ef112936107b
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-01-05 15:48:40 -0600

    uhhh I should probably have commited more often

---
+++ lolbuddy/__init__.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+import json
+from requests_futures.sessions import FuturesSession
+import sys
+import os
+import pickle
+
+def main():
+
+    #for giving a clean exit
+    try:
+        if ('--reset' in sys.argv or '-r' in sys.argv):
+            try:
+                os.remove('.lolbuddy')
+            except:
+                print('\nThere was no pre-existing config file anyway...\n')
+
+        #don't want to write to file if we don't need to
+        writeToFile = False
+
+        try:
+            data = pickle.load(open('.lolbuddy', 'rb'))
+            location = data['location']
+            apiKey = data['apiKey']
+        except:
+            writeToFile = True
+            data = {'location': '', 'apiKey': ''}
+            location = ''
+            apiKey = ''
+
+        #Check if the .lolbuddy file either doesn't have the location saved or doesn't have the correct location saved
+        if (location == '' or not os.path.exists(location)):
+            #otherwise, check if the location is one of the defaults for the OS
+            writeLocation = True
+            if os.name == 'posix': #OS X
+                location = '/Applications/League of Legends.app/Contents/LoL/Config'
+            elif os.name == 'nt': #Windows
+                location = 'C:/Riot Games/League of Legends/Config'
+            else: #Linux
+                print('\nSorry, linux is not supported...\n')
+                quit()
+            #If the location path STILL isn't correct, prompt the user for the correct path
+            while not os.path.exists(location):
+                writeLocation = True
+                location = input('Could not find location of league config folder, please enter it here. It should look something like this "C:/Riot Games/League of Legends/Config" or "/Applications/League of Legends.app/Contents/LoL/Config":\n')
+
+        #if the API key isn't saved or clearly too short to be a champion.gg API key
+        if len(apiKey) < 20: #assuming consistency, champion.gg's is 32 chars
+            writeToFile = True
+            #keep prompting user until they get it right
+            while len(apiKey) < 20:
+                apiKey = input('Not a valid API key, please enter one from api.champion.gg: ')
+
+        if writeToFile:
+            data = {'location': location, 'apiKey': apiKey}
+            print(data)
+            pickle.dump(data, open('.lolbuddy', 'wb'))
+
+        session = FuturesSession()
+
+        apiKeyValidated = False
+        while not apiKeyValidated:
+            url = 'http://api.champion.gg/champion?api_key' + apiKey
+            infoRequest = session.get(url) #start request
+            #requestData = json.loads(infoRequest.result().content.decode('utf-8')) #finish request
+            requestData = infoRequest.result().content.decode('utf-8') #finish request
+            try:
+                champions = json.loads(requestData)
+                apiKeyValidated = True
+            except:
+                apiKey = input('Seems like the API key didn\'t work, please enter a valid API key')
+                data['apiKey'] = apiKey
+                pickle.dump(data, open('.lolbuddy', 'wb'))
+            print(champions)
+            input('')
+        totalChampions = len(champions)
+        championsComplete = 0
+        print('\nThere are currently {0} champions. Getting individual champion data...\n'.format(totalChampions))
+
+        championInfo = []
+
+        def incrementChamps(sess, resp):
+            nonlocal championsComplete
+            championsComplete += 1
+
+        for champion in champions:
+            url = 'http://api.champion.gg/champion/' + champion['key'] + '?&api_key=' + apiKey
+            championData = session.get(url, background_callback=incrementChamps)
+            championInfo.append(championData)
+
+        while championsComplete < totalChampions:
+            print('({0}/{1}) champions complete...'.format(championsComplete, totalChampions), end='\r')
+            sys.stdout.flush()
+
+        print('\n\nAll champion data fetched, creating item sets...')
+
+        for champion in championsInfo:
+            champFolder = location + champion['key']
+            if not os.path.exists(champFolder):
+                os.makedirs(directory)
+
+
+    except (KeyboardInterrupt, SystemExit):
+        print("\n\nShutdown requested...exiting")
+    quit()
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+version = '0.0.13'
+
+setup(
+    name='lolbuddy',
+    version=version,
+    description='a cli tool to update league of legends itemsets and ability order from champion.gg',
+    author='Cyrus Roshan',
+    author_email='hello@cyrusroshan.com',
+    license='MIT',
+    keywords=['lol', 'league', 'league of legends', 'item', 'ability'],
+    url='https://github.com/CyrusRoshan/lolbuddy',
+    packages=find_packages(),
+    package_data={},
+    install_requires=[
+        'requests-futures >= 0.9.5',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lolbuddy=lolbuddy:main',
+        ],
+    },
+)
